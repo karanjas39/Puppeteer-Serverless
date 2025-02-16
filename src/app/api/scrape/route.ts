fbd9a0e46a75e5db566625ebe08eb9d97d07b6d5
karanjas39 <dhillonjaskaran4486@gmail.com>
2025-02-16 08:05:44 +0530

    [fix] new approch

---
+++ src/app/api/scrape/route.ts
@@ -0,0 +1,29 @@
+import chromium from "@sparticuz/chromium";
+import puppeteer from "puppeteer-core";
+
+export const maxDuration = 20;
+chromium.setGraphicsMode = false;
+
+export async function POST(request: Request) {
+  const { url } = await request.json();
+
+  const isLocal = !!process.env.CHROME_EXECUTABLE_PATH;
+
+  const browser = await puppeteer.launch({
+    args: isLocal ? puppeteer.defaultArgs() : chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath:
+      process.env.CHROME_EXECUTABLE_PATH || (await chromium.executablePath()),
+    headless: true,
+  });
+
+  const page = await browser.newPage();
+  await page.goto(url);
+  const pageTitle = await page.title();
+  await browser.close();
+
+  return Response.json({
+    url,
+    pageTitle,
+  });
+}
